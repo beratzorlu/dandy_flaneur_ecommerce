9bda506166bea8b82d7e37f161de623d59124e35
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-07 17:19:10 +0000

    Add template directory settings.

--- dandy_flaneur/settings.py
+++ dandy_flaneur/settings.py
@@ -66,7 +66,10 @@ ROOT_URLCONF = 'dandy_flaneur.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
