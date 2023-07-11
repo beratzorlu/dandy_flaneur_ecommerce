645d52781bc169c2618d65b0b1b84afa3b8bb30f
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-11 16:35:18 +0000

    Create product and category models.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    upc = models.CharField(max_length=50, null=True, blank=True) # universal product code
+    artist = models.CharField(max_length=50, null=True, blank=True)
+    country = models.CharField(max_length=254, unique=True)
+    name = models.CharField(max_length=254, unique=True)
+    description = models.TextField()
+    has_dimentions = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
