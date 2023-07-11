d84c23110dff322d5d3103081b00e191aafc4603
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-11 20:11:36 +0000

    Add store models and fixture data.

--- store/models.py
+++ store/models.py
@@ -18,10 +18,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    upc = models.CharField(max_length=50, null=True, blank=True) # universal product code
+    upc = models.CharField(max_length=50, null=True, blank=True)  # universal product code
     artist = models.CharField(max_length=50, null=True, blank=True)
-    country = models.CharField(max_length=254, unique=True)
-    name = models.CharField(max_length=254, unique=True)
+    country = models.CharField(max_length=254, unique=False, null=True, blank=True)
+    name = models.CharField(max_length=254, unique=True, null=True, blank=True)
     description = models.TextField()
     has_dimentions = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
