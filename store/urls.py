1cfb6601d14ef314bdad7b3f622a46ae9bc4b638
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-11 16:39:18 +0000

    Add products list views and urls.

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allProducts.as_view(), name='store'),
+    path('', views.products_list, name='store'),
 ]
--- store/views.py
+++ store/views.py
@@ -4,8 +4,55 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 
 
-class allProducts(generic.ListView):
-    """ View for rendering the store page """
+def products_list(request):
+    """View for listing all products and sorting."""
 
-    def get(self, request):
-        return render(request, 'store/store_products.html')
+    products = Product.objects.all()
+    categories_list = Category.objects.all()
+    query = None
+    category = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sort_key = request.GET['sort']
+            sort = sort_key
+            if sort_key == 'category':
+                sort_key = 'category__name'
+            if sort_key == 'name':
+                sort_key = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sort_key = f'-{sort_key}'
+            products = products.order_by(sort_key)
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please input search criteria")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
+
+    current_sort = f'{sort}_{direction}'
+
+    context = {
+        'products': products,
+        'categories_list': categories_list,
+        'search_term': query,
+        'current_sort': current_sort
+    }
+
+    return render(request, 'store/store_products.html', context)
