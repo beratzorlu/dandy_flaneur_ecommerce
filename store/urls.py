b18ab2c388c9026a97696141014bfd586d1eacee
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-12 13:42:14 +0000

    Add urls for edit_store view.

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.products_list, name='store'),
     path('<int:product_id>/', views.store_detail, name='store_detail'),
     path('add_store/', views.add_store, name='add_store'),
+    path('delete_store/<int:product_id>/', views.delete_store, name='delete_store'),
+    path('edit_store/<int:product_id>/', views.edit_store, name='edit_store'),
 ]
