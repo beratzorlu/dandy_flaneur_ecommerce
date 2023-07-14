576cd838dfa82ac4835d8185653223e696835418
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-14 15:01:42 +0000

    Add context declaration to view_basket view.

--- basket/views.py
+++ basket/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.contrib import messages
-from store.models import Product
+from store.models import Product, Category
 
 
 def view_basket(request):
     """Renders the basket contents page"""
+
+    products = Product.objects.all()
+    all_categories = Category.objects.all()
+
+    context = {
+        'products': products,
+        'all_categories': all_categories
+    }
 
     return render(request, 'basket/basket.html')
