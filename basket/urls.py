062a5b27cddfc8b1ee53ae633a311e709ae54b66
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-14 19:06:58 +0000

    Add remove basket url.

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
     path('add_basket/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('edit_basket/<item_id>/', views.edit_basket, name='edit_basket'),
+    path('remove_basket/<item_id>/', views.remove_basket, name='remove_basket'),
 ]
