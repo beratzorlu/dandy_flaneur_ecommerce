5c51894e73e8aa20de47bb6cdc59e6247bc67a72
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-14 16:06:50 +0000

    Add global variables for delivery fee calculation.

--- dandy_flaneur/settings.py
+++ dandy_flaneur/settings.py
@@ -85,6 +85,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_items',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
@@ -189,3 +190,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 100
