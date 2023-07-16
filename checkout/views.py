53748e2c3ff0f742c44c3b9f0898577f591b8330
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-16 23:18:13 +0000

    Add initial code for storeCheckout and checkout_data_cache views.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import (render, redirect, reverse, get_object_or_404,
+                              HttpResponse)
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout_data_cache(request):
+    pass
+
+
+def storeCheckout(request):
+    """Renders checkout form page and logic"""
+
+    template = 'checkout/checkout.html'
+    order_form = OrderForm()
+    context = {
+        'order_form': order_form,
+    }
+    return render(request, template, context)
