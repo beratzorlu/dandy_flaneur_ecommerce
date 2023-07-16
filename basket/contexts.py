07fff7df8835a1ba1e518f217392f8a03632dbe6
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-16 12:08:01 +0000

    Change variables to support app models and views.

--- basket/contexts.py
+++ basket/contexts.py
@@ -23,14 +23,14 @@ def basket_items(request):
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data['items_by_size'].items():
+            for dimentions, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
                 basket_contents.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
-                    'size': size,
+                    'dimentions': dimentions,
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
