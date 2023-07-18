5fb8a604911396d6e9cedcd616c4a9dbdbfa7ecc
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-18 14:31:36 +0000

    Add url for profiles app.

--- dandy_flaneur/urls.py
+++ dandy_flaneur/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('workshops/', include('workshop.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
