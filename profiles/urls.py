3e90fa4e0d0d96835c25ad809149a040bf20748e
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-18 16:55:44 +0000

    Add link to account order history.

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profiles, name='profiles'),
+    path('account_order_history/<order_number>', views.account_order_history, name='account_order_history'),
 ]
