63c7016f2f1665374fd478cc168060e7df1ac1b0
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-18 14:31:04 +0000

    Add view to render profilees template.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+
+def profiles(request):
+    """ Display the user's profile. """
+
+    template = 'profiles/profiles.html'
+    context = {}
+
+    return render(request, template, context)
