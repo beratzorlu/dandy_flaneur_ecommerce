fc50bc8934efe7717c4903993b7787a8cf227dfc
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-18 14:32:15 +0000

    Change variable name on model to a custom one.

--- checkout/models.py
+++ checkout/models.py
@@ -9,7 +9,7 @@ from profiles.models import AccountProfile
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(AccountProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
+    account_profile = models.ForeignKey(AccountProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
