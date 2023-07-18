c70887e317364be15ba1edd4f2c0c65dbcc605a7
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-18 16:56:23 +0000

    Add view to render account order history.

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import AccountProfile
 from .forms import AccountProfileForm
+from checkout.models import Order
 
 
 def profiles(request):
@@ -22,6 +23,23 @@ def profiles(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def account_order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
