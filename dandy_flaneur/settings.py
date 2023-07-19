f8777ffee785e6cae73a920ddbd0b35dd90ab233
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-19 19:29:45 +0000

    Fix heroku deployment issue.

--- dandy_flaneur/settings.py
+++ dandy_flaneur/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from pathlib import Path
 import os
 import dj_database_url
-from pathlib import Path
 if os.path.isfile('env.py'):
     import env
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
