1555f5ce91a433bf25575aa853d8b52f5c30ee28
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-20 13:00:22 +0000

    Add Gmail configurations.

--- dandy_flaneur/settings.py
+++ dandy_flaneur/settings.py
@@ -123,10 +123,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
 # Allauth Authentication Settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 
@@ -217,7 +213,6 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'dandyflaneur@email.com'
 
 # AWS Configs
 
@@ -245,3 +240,15 @@ if 'USE_AWS' in os.environ:
     # Static and media URLs override for production version
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'dandyflaneurcommerce@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
