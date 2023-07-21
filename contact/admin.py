e3cf3364ae3ae56db7e773342cfaeacb5b393be0
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-21 14:39:16 +0000

    Add admin view for contact us data.

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from daterangefilter.filters import DateRangeFilter
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_filter = ('user', 'sender_name', 'email_address',
+                   'phone_num', 'date_created')
+    list_display = ('form_id', 'user', 'sender_name',
+                    'message', 'phone_num', 'date_created')
+
+    search_fields = ['sender_name']
+    list_filter = (('sender_name', DateRangeFilter),)
