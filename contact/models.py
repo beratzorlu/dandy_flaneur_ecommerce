d3dd3c5e5cb46afb966e840801651a6aa53de893
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-21 14:40:39 +0000

    Add Contact data model and class methods.

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Contact form model
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="form_contact_user", null=True)
+    form_id = models.AutoField(primary_key=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    sender_name = models.CharField(max_length=50, null=True)
+    email_address = models.EmailField(max_length=254, default="")
+    phone_num = PhoneNumberField(null=True)
+    message = models.TextField()
+
+    class Meta:
+        ordering = ['date_created']
+
+    def __str__(self):
+        return self.sender_name
