65b1a53c1580fb644b5653d0a463b342fcd158f8
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-21 23:15:40 +0000

    Update django email settings.

--- dandy_flaneur/settings.py
+++ dandy_flaneur/settings.py
@@ -248,7 +248,7 @@ if 'USE_AWS' in os.environ:
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
-# Gmail emails configs 
+# Gmail emails configs
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'dandyflaneurcommerce@gmail.com'
@@ -260,4 +260,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
