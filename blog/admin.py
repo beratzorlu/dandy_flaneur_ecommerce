a6a1ea23eed37df30c2b12b8dee079a4e094d559
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-24 16:45:33 +0000

    Add admin form form and django-summernote.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('author', 'title', 'slug', 'created_on', 'is_published')
+    list_filter = ('author', 'is_published', 'created_on', 'is_published')
