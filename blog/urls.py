9a2e869a87e96b01a8a83504bf7a314488d7c9d9
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-24 22:41:18 +0000

    Add blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.BlogList.as_view(), name='blog_list'),
     path('summernote/', include('django_summernote.urls')),
+    path('<slug:slug>/', views.PostDetail.as_view(), name="blog_detail")
 ]
