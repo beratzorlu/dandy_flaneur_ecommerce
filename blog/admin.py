b3724a927e4889a4380717073395638736881856
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-26 01:40:54 +0000

    Add custom admin actions.

--- blog/admin.py
+++ blog/admin.py
@@ -10,3 +10,14 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('author', 'title', 'slug', 'created_on', 'is_published')
     list_filter = ('author', 'is_published', 'created_on', 'is_published')
+
+    def make_published(modeladmin, request, queryset):
+        queryset.update(is_published=True)
+    
+    def make_unpublished(modeladmin, request, queryset):
+        queryset.update(is_published=False)
+        
+    make_published.short_description = "Publish"
+    make_unpublished.short_description = "Unpublish"
+
+    actions = [make_published, make_unpublished]
