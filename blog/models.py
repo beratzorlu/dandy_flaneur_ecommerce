418e437bb1bdefb1e6c8c0fbce6693863f7d848f
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-26 01:43:11 +0000

    Add get_absolute_url for succesful blog CRUD ops.

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -19,6 +20,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog_list')
 
     def number_of_likes(self):
         return self.likes.count()
