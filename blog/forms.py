e54b8cd16aa3abbee6eb9409a6f08825efae4b96
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-26 01:42:23 +0000

    Update docstring and fix syntax.

--- blog/forms.py
+++ blog/forms.py
@@ -1,18 +1,18 @@
-from .models import Comment, Post
-from products.widgets import CustomClearableFileInput
+from .models import Post
+from store.widgets import CustomClearableFileInput
 from django_summernote.widgets import SummernoteWidget
 from django_summernote.fields import SummernoteTextFormField
 from django import forms
 
 
-class PostForm(forms.ModelForm):
+class BlogForm(forms.ModelForm):
     """
-    Renders form for editing and publishing new post.
+    Renders form for editing and publishing a post.
     """
     class Meta:
         model = Post
         fields = ('title', 'slug', 'content',
-                  'excerpt', 'image', 'is_published',)
+                  'excerpt', 'image', 'is_published')
 
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
     content = forms.CharField(widget=SummernoteWidget())
