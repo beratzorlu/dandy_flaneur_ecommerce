b2be2aa01e84ff4d95e456307d993db1f09560d2
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-26 01:45:19 +0000

    Add blog add and delete urls.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.BlogList.as_view(), name='blog_list'),
     path('summernote/', include('django_summernote.urls')),
-    path('<slug:slug>/', views.PostDetail.as_view(), name="blog_detail")
+    path('add/', views.AddBlog.as_view(), name="blog_add"),
+    path('<slug:slug>/delete', views.DeleteBlog.as_view(), name='blog_delete'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name="blog_detail"),
 ]
