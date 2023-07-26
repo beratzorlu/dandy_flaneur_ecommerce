8bde070e376db474166097a8b61e3675fd9dde61
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-26 22:06:19 +0000

    Add url path for post like view.

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('add/', views.AddBlog.as_view(), name="blog_add"),
     path('<slug:slug>/delete', views.DeleteBlog.as_view(), name='blog_delete'),
-     path('<slug:slug>/edit', views.EditBlog.as_view(), name='blog_edit'),
+    path('<slug:slug>/edit', views.EditBlog.as_view(), name='blog_edit'),
+    path('<slug:slug>/like', views.LikeBlog.as_view(), name='blog_like'),
     path('<slug:slug>/', views.PostDetail.as_view(), name="blog_detail"),
 ]
