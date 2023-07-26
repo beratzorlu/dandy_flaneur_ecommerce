fad47c525c4c1cade5524ecb6a68292c8b8a2a64
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-26 16:18:06 +0000

    Add edit blog form.

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
-from .models import Post
 from store.widgets import CustomClearableFileInput
+from .models import Post
 from django_summernote.widgets import SummernoteWidget
 from django_summernote.fields import SummernoteTextFormField
 from django import forms
@@ -13,6 +13,30 @@ class BlogForm(forms.ModelForm):
         model = Post
         fields = ('title', 'slug', 'content',
                   'excerpt', 'image', 'is_published')
+
+    widgets = {
+            'title': forms.TextInput(attrs={'class': 'mb-4 rounded-0 dandy-border form-control'}),
+            'excerpt': forms.TextInput(attrs={'class': 'mb-4 rounded-0 dandy-border form-control'}),
+            'content': forms.Textarea(attrs={'class': 'mb-4 rounded-0 dandy-border form-control'}),
+        }
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    content = forms.CharField(widget=SummernoteWidget())
+
+
+class EditForm(forms.ModelForm):
+    """
+    Defines the model for the blog edit form
+    """
+    class Meta:
+        model = Post
+        fields = ('is_published', 'title', 'excerpt', 'content', 'image')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'mb-4 rounded-0 dandy-border form-control'}),
+            'excerpt': forms.TextInput(attrs={'class': 'mb-4 rounded-0 dandy-border form-control'}),
+            'content': forms.Textarea(attrs={'class': 'mb-4 rounded-0 dandy-border form-control'}),
+        }
 
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
     content = forms.CharField(widget=SummernoteWidget())
