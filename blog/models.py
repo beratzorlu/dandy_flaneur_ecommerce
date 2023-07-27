3b47fdc23eb6bb0a27d4d6b448bd829219d15cb4
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-27 00:39:41 +0000

    Add comment model.

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    """
+    Defines the db models for blog posts.
+    """
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_blog")
     is_liked = models.ManyToManyField(User, related_name='post_like', blank=True)
     title = models.CharField(max_length=200, unique=True)
@@ -26,3 +29,23 @@ class Post(models.Model):
 
     def number_of_likes(self):
         return self.is_liked.count()
+
+
+class Comment(models.Model):
+    """
+    Defines the db models for comment posts.
+    """
+    name = models.CharField(max_length=32)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comment_author', )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"{self.content} | {self.name}"
