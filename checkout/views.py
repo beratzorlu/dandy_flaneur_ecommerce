318a572c8651b9f1ac758ac6a87820286ddea8f1
berat.zorlu <berat.zorlu@hotmail.com>
2023-07-30 19:00:30 +0000

    Add test cases for checkout app.

--- checkout/views.py
+++ checkout/views.py
@@ -92,7 +92,7 @@ def store_checkout(request):
         basket = request.session.get('basket', {})
         if not basket:
             messages.error(request, "There's nothing in your basket at the moment")
-            return redirect(reverse('products'))
+            return redirect(reverse('store'))
 
         current_basket = basket_items(request)
         total = current_basket['grand_total']
