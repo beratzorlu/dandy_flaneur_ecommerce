b97e238132e9ebb0a7cc7e0442447c8e20d64730
berat.zorlu <berat.zorlu@hotmail.com>
2023-08-02 11:03:46 +0000

    Remove Workshop app due to redundanncy.

--- dandy_flaneur/settings.py
+++ dandy_flaneur/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'blog',
     'contact',
     'store',
-    'workshop',
     'basket',
     'checkout',
     'profiles',
--- workshop/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.allWorkshops.as_view(), name='workshops'),
-]
